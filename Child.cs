7b50384786996bc3d25199fca08764226f24c409
Peter42306 <pzalizko@gmail.com>
2023-09-19 12:46:33 +0300

    HW 2023 06 24

---
+++ Child.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAppHW20230624
+{    
+    public partial class Child : Form
+    {
+        // ссылка на родителя
+        Parent parent = null;
+
+        // делегат для передачи текста из дочерней формы в родительскую
+        public delegate void ChildTextChangedEventHandlerWithDelegate(string newText);
+
+        // событие для делегата
+        public event ChildTextChangedEventHandlerWithDelegate ChildTextChangedWithDelegate;
+
+        public Child()
+        {
+            InitializeComponent();
+        }
+
+        // Конструктор, принимающий родительскую форму
+        public Child(Parent parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+            textBox1.TextChanged += ChildTextBox1_TextChanged; 
+            parent.ParentTextBoxChangedWithDelegate += ParentTextBox1ChangedWithDelegate; // Подписываемся на событие ParentTextChangedWithDelegate родительской формы
+        }
+
+        // Обработчик события изменения текста в textBox1 дочерней формы
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string childText = textBox1.Text;
+            parent.ParentTextBox1Text = childText;
+            ChildTextChangedWithDelegate?.Invoke(childText); // Вызываем событие ChildTextChangedWithDelegate и передаем в него текущий текст
+        }
+
+        // Обработчик события изменения текста в textBox1 дочерней формы
+        private void ChildTextBox1_TextChanged(object? sender, EventArgs e)
+        {
+            string newText = textBox1.Text;
+            ChildTextChangedWithDelegate?.Invoke(newText); // Вызываем событие ChildTextChangedWithDelegate и передаем в него новый текст
+        }
+
+        // Обработчик события изменения текста в родительской форме
+        private void ParentTextBox1ChangedWithDelegate(string parentText)
+        {
+            textBox1.Text = parentText;
+        }                
+    }
+}
---
+++ Parent.cs
@@ -0,0 +1,52 @@
+﻿namespace WinFormsAppHW20230624
+{
+    public partial class Parent : Form
+    {
+        // делегат для создания события, для передачи текста из родительской формы в дочернюю форму
+        public delegate void ParentTextBoxChangedEventHandlerWithDelegate(string parentText);
+
+        // событие этого делегата
+        public event ParentTextBoxChangedEventHandlerWithDelegate ParentTextBoxChangedWithDelegate;
+
+        // даём доступ к полю textBox1.Text родиельской формы
+        public string ParentTextBox1Text
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+        public Child Child { get; set; }
+        public Parent()
+        {
+            InitializeComponent();
+
+            // Получаем размер экрана
+            Screen screen = Screen.FromControl(this);
+            int screenWidth = screen.Bounds.Width;
+            int screenHeight = screen.Bounds.Height;
+
+            // Устанавливаем начальное положение родительской формы чуть левее от центра
+            int parentX = (screenWidth - Width) / 4; // 1/4 ширины экрана
+            int parentY = (screenHeight - Height) / 2; // по центру по вертикали
+            Location = new Point(parentX, parentY);
+
+            // Создаем и отображаем дочернюю форму
+            Child = new Child(this);
+            Child.StartPosition = FormStartPosition.Manual;
+            Child.Location=new Point(parentX +Width, parentY);
+            Child.Show();
+
+            textBox1.TextChanged += ParentTextBox1TextChanged; // устанавливаем обработчик события TextChanged для textBox1
+        }
+
+        // при изменениях в textBox1 вызывается событие ддя делегата
+        private void ParentTextBox1TextChanged(object? sender, EventArgs e)
+        {
+            string parentText = textBox1.Text;
+            ParentTextBoxChangedWithDelegate?.Invoke(parentText);
+        }
+
+        private void Parent_Load(object sender, EventArgs e) {}
+        private void textBox1_TextChanged(object sender, EventArgs e) {}
+    }
+}
